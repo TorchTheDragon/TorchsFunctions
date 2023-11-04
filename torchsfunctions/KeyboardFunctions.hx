3a8edd56d7f3b464eefe06152d9941a37ff405f3
TorchTheDragon <72819066+TorchTheDragon@users.noreply.github.com>
2023-11-03 21:07:10 -0400

    Added a keyboard functions file

---
+++ torchsfunctions/KeyboardFunctions.hx
@@ -0,0 +1,31 @@
+package torchsfunctions;
+
+import flixel.FlxG;
+import flixel.input.keyboard.FlxKey;
+
+class KeyboardFunctions{
+    /**
+     * Just a simple function to determine which key was pressed. Good for sequential keypresses. An example of how to use this is by simply adding the value to a string in the update function.
+     * 
+     * Ex: 
+     * ```
+     * public var value:String = '';
+     * override function update(elapsed:Float) {
+     *      value += keypressToString(); // This will add a key to the string everytime a key is pressed
+     * }
+     * ```
+     * @return Key that was pressed as a String
+     */
+    public static function keypressToString():String
+    {
+        var characterToAdd:String = "";
+        if (FlxG.keys.justPressed.ANY) {
+            final key = cast(FlxG.keys.firstJustPressed(), FlxKey);
+            if (key != FlxKey.NONE){
+                final i = key.toString().toUpperCase();
+                characterToAdd += FlxKey.fromStringMap.get(i);
+            }
+        }
+        return characterToAdd;
+    }
+}
